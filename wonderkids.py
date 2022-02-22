c7f3a74062669fc1778e4a4f1b726310043d045f
Manstraa <taylanozdemirciq7@gmail.com>
2022-02-22 23:49:00 +0300

    taylans scoutlist

---
+++ wonderkids.py
@@ -0,0 +1,79 @@
+# coding=utf-8
+# HELLO MY FUTBOL WORLD
+# MY FUTBOL WORLD'S RULES
+# 1 = GoalKeeper
+# 2 = Defense
+# 3 = Midfielder
+# 4 = Striker
+
+def w_kids () :
+    girdi = input('Enter Position Number :')
+    if girdi ==1:
+        print("1. Ersin Destanoglu - (BESIKTAS)   "
+              "2. Ivan Martinez (Castellon)   "
+              "3. Lucas Canizares (Real Madrid)    "
+              "4.Diogo Pinto (Sporting)    "
+              "5. Berke Ozer (Fenerbahce)    "
+              "6. Arijanet Muric (Demirspor-Man City)   "
+              "7.Dogan Alemdar (Stade Rennes)")
+    if girdi==2:
+        print("1. Nuno Mendes (PSG)  "
+              "2. Eric Garcia (Barcelona)  "
+              "3. Ronald Araujo (Barcelona)  "
+              "4.Ravil Tagir (Basaksehir)  "
+              "5. Serdar Saatci (Besiktas)  "
+              "6. Goncalo Inacio (Sporting)   "
+              "7. Koray Gunter (Hellas Verona)  "
+              "8. Josko Gvardiol (Leipzig)   "
+              "9. Rayan Ait Nouri (Wolves)   "
+              "10. Luca Netz (Monchengladbach)  "
+              "11. Brandon Williams (Man United - Norwich City)  "
+              "12. Rıdvan Yilmaz (Besiktas)   "
+              "13.Tino Livramento (Southampton)   "
+              "14. Jurrien Timber (Ajax)    "
+              "15. Sergino Dest (Barcelona)   "
+              "16. Tariq Lamptey (Brighton)   "
+              "17. (Alphonso Davies (Bayern Munchen)")
+    elif girdi ==3:
+            print("1. Pedri (Barcelona)  "
+                  "2. Bellingham (Dortmund)  "
+                  "3. Gallagher (Crystal Palace)  "
+                  "4. Luca Waldschmit (Wolfsburg)   "
+                  "5. Moises Caicedo (Brighton)   "
+                  "6. Nicolo Rovella (Zebre)   "
+                  "7. Weston Mckennie (Juventus)   "
+                  "8. Nico (Barcelona)   "
+                  "9. Eduardo Camavinga (Real Madrid)    "
+                  "10. Ilaix Moria (Leipzig)   "
+                  "11. Dominik Szoboszlai (Leipzig)  "
+                  "12. Gavi (Barcelona)    "
+                  "13. Xavi Simons (PSG)   "
+                  "14. Emirhan Ilkhan (Besiktas)     "
+                  "15. Emirhan Delibas (Besiktas)    "
+                  "16. Demir Ege Tiknaz (Besiktas)   "
+                  "17. Can Bozdogan (Besiktas)     "
+                  "18. Aurelien Tchouameni (Monaco)")
+    if girdi==4:
+            print ("1.Jack Grealish (Man City) [ I now he is not wonderkid but he is my son]    "
+                   "2. Ansu Fati (Barcelona)   "
+                   "3. Curtis Jones (Liverpool)   "
+                   "4. Bukayo Saka (Arsenal)   "
+                   "5. Gabriel Martinelli (Arsenal) "
+                   "6. Jeremy Doku (Rennes)    "
+                   "7. Bryan Gil (Tottenham)   "
+                   "8. Noa Lang (Club Brugge)    "
+                   "9. Yeremi Pino (Villareal)   "
+                   "10. Rodrigo (Real Madrid)    "
+                   "11. Cole Palmer (Man City)    "
+                   "12. Ajdin Hasic (Besiktas)   "
+                   "13. Jamal Musical (Bayern)    "
+                   "14. Yusuf Demir (Rapid Wien)    "
+                   "15. Florian Wirtz (Bayer Leverkusen)    "
+                   "16. Emile Smith Rowe (Arsenal)    "
+                   "17. Burak Ince (Altınordu)     "
+                   "18. Michael Olise (Crystal Palace)   "
+                   "19. Youssoufa Moukoko (Dortmund)   "
+                   "20. Fabio Silva (Wolves)   " 
+                   "21. Ali Akman (Frankfurt)    "
+                   "22. Karim Adeyemi (Salzburg)")
+print (w_kids())
